9cc9541a148d9f26719d6bca76884ec30164b30a
Alex <sp00king@gmail.com>
2021-05-27 22:49:02 +0300

    try

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,8 @@ export function App() {
         <Switch>
           <Route path="/tenner" component={TennerApp} />
         </Switch>
-        <img className="hero img" src="https://cdn.pixabay.com/photo/2020/01/08/03/56/man-4749237_960_720.jpg"></img>
+        {/* <img className="hero img" src="https://cdn.pixabay.com/photo/2020/01/08/03/56/man-4749237_960_720.jpg"></img> */}
+        <img className="hero img" src="https://cdn.pixabay.com/photo/2019/04/25/14/43/workplace-4155023_960_720.jpg"></img>
         <div className="main-container">
           <h1>This is main, here we render the gig list with gig previews and mucho nice content that will help us get jobs</h1>
         </div>
