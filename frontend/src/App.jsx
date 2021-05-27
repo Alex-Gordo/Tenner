033dafd7125d227f47fc1cef2a05a1f167e10255
Alex <sp00king@gmail.com>
2021-05-27 19:24:56 +0300

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { TennerApp } from './pages/TennerApp';
-import { gigService} from '../../frontend/src/services/gigService.js';
-import { AppHeader} from './cmps/AppHeader.jsx';
+import { gigService } from '../../frontend/src/services/gigService.js';
+import { AppHeader } from './cmps/AppHeader.jsx';
 
 
 export function App() {
   return (
     <div className="app">
       <Router>
-      <AppHeader/>
+        <AppHeader />
 
-        </header>
         <main className="app">
           <Switch>
             <Route path="/tenner" component={TennerApp} />
