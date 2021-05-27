d3a88e0898a8e7678220fdbec7436d0735b2ae8c
Alex <sp00king@gmail.com>
2021-05-27 15:45:31 +0300

    json typo fix

---
+++ frontend/src/store/actions/gigActions.js
@@ -0,0 +1,13 @@
+import { gigService } from '../../services/gigService'
+
+export function loadGigs(filterBy = {
+    keyword: '',
+    description: '',
+    category: '',
+    features: '',
+    sortBy: '',
+}) {
+
+
+
+}
