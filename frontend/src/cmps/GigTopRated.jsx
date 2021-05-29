be2c9374331b53a7c53c3cf2768a6327c2641e98
Alex <sp00king@gmail.com>
2021-05-29 16:19:17 +0300

    rendering top gigs in home

--- frontend/src/cmps/GigTopRated.jsx
+++ frontend/src/cmps/GigTopRated.jsx
@@ -5,13 +5,17 @@ import { GigPreview } from '../cmps/GigPreview.jsx';
 export function GigTopRated({ gigs }) {
     console.log('gigs', gigs);
 
+    // TODO: filter top gigs and slice them to get 4 top gigs
+    const topGigs = gigs.slice(0, 4)
+    console.log('topGigs', topGigs);
+
     return (
         <React.Fragment>
+            <h1>Top rated gigs:</h1>
             <main className="gig-list flex">
-                {/* {gigs.map(gig => {
+                {topGigs.map(gig => {
                     return <GigPreview key={gig._id} gig={gig} />
-                })} */}
-                Top rated gigs rendering
+                })}
             </main>
         </React.Fragment>
     )
