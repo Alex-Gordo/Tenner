2bc9fea93877a54fc353d891ef3b01e63740269d
Alex <sp00king@gmail.com>
2021-06-02 23:25:00 +0300

    explore css

--- frontend/src/services/gigService.js
+++ frontend/src/services/gigService.js
@@ -13,13 +13,11 @@ export const gigService = {
 var gGigs = require('../data/tenner.json')
 const KEY = 'gigs'
 
-console.log('gGigs', gGigs);
 // // const BASE_URL = process.env.NODE_ENV === 'my-app/src/services/gigs.json'
 
 async function query(filterBy) {
     storageService.saveToStorage(KEY, gGigs)
     const gigs = await storageService.getFromStorage(KEY)
-
     if (!filterBy) return Promise.resolve(gigs.gig)
     if (!gGigs) return storageService.getFromStorage(KEY)
         .then((gigs) => {
@@ -27,7 +25,6 @@ async function query(filterBy) {
             return gigs.gig
         });
     const filterRegex = new RegExp(filterBy, 'i')
-    console.log('gigigigigigis', gigs);
     return Promise.resolve(gigs.gig.filter(gig => filterRegex.test(gig.category)))
 };
 
