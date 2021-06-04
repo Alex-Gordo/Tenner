caca431554f52e6375ef3ea560503dc5ea60365b
Arik Alexandrov <arikxl@gmail.com>
2021-06-04 11:36:00 +0300

    arik imgs

--- frontend/src/cmps/GigCategories.jsx
+++ frontend/src/cmps/GigCategories.jsx
@@ -10,10 +10,18 @@ export function GigCategories() {
         <section className="categories">
             <h3>Popular professional services:</h3>
             <div className="gig-list flex">
-                <NavLink to="/gigs?q=design" className="cat-link"><img className="cat-img" src={design} alt="design" /> <p className="cat-title">Design</p></NavLink>
-                <NavLink to="/gigs?q=digital" className="cat-link"><img className="cat-img" src={digital} alt="digital" /><p className="cat-title">Digital</p></NavLink>
-                <NavLink to="/gigs?q=programming" className="cat-link"><img className="cat-img" src={programming} alt="programming" /><p className="cat-title">Programming</p></NavLink>
-                <NavLink to="/gigs?q=video" className="cat-link"><img className="cat-img" src={video} alt="video" /><p className="cat-title">Video</p></NavLink>
+                <NavLink to="/gigs?q=design" className="cat-link">
+                    <img className="cat-img" src={'https://res.cloudinary.com/arikxl/image/upload/v1622785624/tenner/category/design_kx3xor.jpg'} alt="design" />
+                     <p className="cat-title">Design</p></NavLink>
+                <NavLink to="/gigs?q=digital" className="cat-link">
+                    <img className="cat-img" src={'https://res.cloudinary.com/arikxl/image/upload/v1622785624/tenner/category/digital_cvqyqy.jpg'} alt="digital" />
+                    <p className="cat-title">Digital</p></NavLink>
+                <NavLink to="/gigs?q=programming" className="cat-link">
+                    <img className="cat-img" src={'https://res.cloudinary.com/arikxl/image/upload/v1622785624/tenner/category/programming_fkngg2.jpg'} alt="programming" />
+                    <p className="cat-title">Programming</p></NavLink>
+                <NavLink to="/gigs?q=video" className="cat-link">
+                    <img className="cat-img" src={'https://res.cloudinary.com/arikxl/image/upload/v1622785624/tenner/category/video_ufihzo.jpg'} alt="video" />
+                    <p className="cat-title">Video</p></NavLink>
             </div>
         </section>
     )
