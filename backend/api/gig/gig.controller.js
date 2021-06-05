639a393109016cde864712f1956889cf44e63d2f
Alex <sp00king@gmail.com>
2021-06-05 15:16:42 +0300

    basic backend

---
+++ backend/api/gig/gig.controller.js
@@ -0,0 +1,65 @@
+const logger = require('../../services/logger.service')
+const userService = require('../user/user.service')
+const socketService = require('../../services/socket.service')
+const gigService = require('./gig.service')
+
+async function getGigs(req, res) {
+    try {
+        const gigs = await gigService.query(req.query)
+        res.send(gigs)
+    } catch (err) {
+        logger.error('Cannot get gigs', err)
+        res.status(500).send({ err: 'Failed to get gigs' })
+    }
+}
+
+async function deleteGig(req, res) {
+    try {
+        await gigService.remove(req.params.id)
+        res.send({ msg: 'Deleted successfully' })
+    } catch (err) {
+        logger.error('Failed to delete gig', err)
+        res.status(500).send({ err: 'Failed to delete gig' })
+    }
+}
+
+
+async function addGig(req, res) {
+    try {
+        var gig = req.body
+        gig.byUserId = req.session.user._id
+        gig = await gigService.add(gig)
+
+        // prepare the updated review for sending out
+        gig.byUser = await userService.getById(gig.byUserId)
+        gig.aboutUser = await userService.getById(gig.aboutUserId)
+
+        console.log('CTRL SessionId:', req.sessionID);
+        socketService.broadcast({ type: 'gig-added', data: gig })
+        socketService.emitToAll({ type: 'user-updated', data: gig.byUser, room: req.session.user._id })
+        res.send(gig)
+
+    } catch (err) {
+        console.log(err)
+        logger.error('Failed to add gig', err)
+        res.status(500).send({ err: 'Failed to add gig' })
+    }
+}
+
+async function getGig(req, res) {
+    try {
+        const gig = await gigService.getById(req.params.id);
+        res.send(gig);
+    } catch (err) {
+        logger.error('Failed to get gig', err);
+        res.status(500).send({ err: 'Failed to get gig' });
+    }
+}
+
+
+module.exports = {
+    getGigs,
+    deleteGig,
+    addGig,
+    getGig
+}
--- backend/services/db.service.js
+++ backend/services/db.service.js
@@ -1,4 +1,5 @@
 const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb+srv://tennerapp:tennerapp@cluster0.romtt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const config = require('../config')
 
@@ -6,13 +7,15 @@ module.exports = {
     getCollection
 }
 
-
+const dbURL = 'mongodb://localhost:27017';
+const dbName = 'tenner_db'
 var dbConn = null
 
 async function getCollection(collectionName) {
     try {
         const db = await connect()
         const collection = await db.collection(collectionName)
+        console.log(collection)
         return collection
     } catch (err) {
         logger.error('Failed to get Mongo collection', err)
@@ -23,11 +26,12 @@ async function getCollection(collectionName) {
 async function connect() {
     if (dbConn) return dbConn
     try {
-        const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-        const db = client.db(config.dbName)
+        const client = await MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+        const db = client.db(dbName)
         dbConn = db
         return db
     } catch (err) {
+        console.log('cannot connect to DB');
         logger.error('Cannot Connect to DB', err)
         throw err
     }
